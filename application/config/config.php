ad910abfef342caa136788b4c43e04ed414d2a6a
mamat08nurahmat <mamat08nurahmat@gmail.com>
2019-03-08 17:09:33 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/testing/';
+//$config['base_url'] = 'http://localhost/testing/';
+$config['base_url'] = '34.222.222.131/racode/';
 
 /*
 |--------------------------------------------------------------------------
@@ -367,6 +368,7 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
+/*
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
@@ -374,6 +376,14 @@ $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
+*/
+
+$config['sess_driver'] = 'database';
+$config['sess_cookie_name'] = 'ci_sessions';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = 'ci_sessions';//its your table name name
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
 
 /*
 |--------------------------------------------------------------------------
